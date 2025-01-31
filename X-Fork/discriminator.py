2351cd05619460d6023cfdf2b6d8821a764994ef
Alessandro <alexdeluca1103@gmail.com>
2025-01-31 16:56:33 +0100

    fixes

--- X-Fork/discriminator.py
+++ X-Fork/discriminator.py
@@ -19,26 +19,26 @@ class Discriminator(nn.Module):
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
+
+        batch_size = x.shape[0]
+
         e1 = self.encoder1(x)
-        print(f"e1 shape: {e1.shape}")
         e2 = self.encoder2(e1)
-        print(f"e2 shape: {e2.shape}")
         e3 = self.encoder3(e2)
-        print(f"e3 shape: {e3.shape}")
         e4 = self.encoder4(e3)
-        print(f"e4 shape: {e4.shape}")
         e5 = self.encoder5(e4)
-        print(f"e5 shape: {e5.shape}")
         e6 = self.encoder6(e5)
-        print(f"e6 shape: {e6.shape}")
         e7 = self.encoder7(e6)
-        print(f"e7 shape: {e7.shape}")
         
         out = self.final_conv(e7)
         out = self.sigmoid(out)
         
-        print(f"out shape: {out.shape}")
-        
+        assert out.shape == (batch_size, 1, 1, 1), f"Unexpected shape for discriminator output: {out.shape}"
+
+        out = out.view(batch_size, -1)
+
+        assert out.shape == (batch_size, 1), f"Unexpected shape for discriminator output: {out.shape}"
+
         return out
 
 
@@ -48,7 +48,5 @@ if __name__ == "__main__":
     input_image = torch.randn(batch_size, input_channels, 512, 512)
     model = Discriminator(input_channels)
     output = model(input_image)
-
-    assert output.shape == (batch_size, 1, 1, 1), f"Unexpected shape for discriminator output: {output.shape}"
 
     print("Test passed: Discriminator outputs correct shape.")
